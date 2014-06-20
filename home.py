dd92d70b06fed05a0185b6364129c5ff7a36ad26
cliffkimani <cliffkimani@gmail.com>
2014-06-20 09:57:00 +0300

    add indexes

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ __author__ = 'ian'
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-
+#indexing now
 
 def index(request):
     return render_to_response('index.html')
