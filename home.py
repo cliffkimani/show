c4fe97d1c864c75b51346db73d6a84207df705bd
cliff mwenja <cliffkimani@gmail.com>
2014-07-07 16:54:02 +0300

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,6 @@ __author__ = 'ian'
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 #indexing now
-#fdghj
 
 def index(request):
     return render_to_response('index.html')
